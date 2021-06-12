11291f39f6ba49620fcb18a0d3d61ff1152f5307
Srikar <srikarsamanchi1999@gmail.com>
2021-06-12 10:05:59 +0530

    Added: giving choice to player

--- TicTacToeGame/src/WorkShop/src/com/bridgeLabz/workShop/TicTacToeGame.java
+++ TicTacToeGame/src/WorkShop/src/com/bridgeLabz/workShop/TicTacToeGame.java
@@ -10,11 +10,32 @@ public class TicTacToeGame
     	//inbuilt method
     	Arrays.fill(board, ' ');
     }
+    //assigning values for computer and player
+    public static char playerAndComputerChoice(char player,char computer)
+    {
+    	if(player == 'o')
+    		computer='x';
+    	else
+    		computer='o';
+    	return computer;
+    	
+    }
     public static void main(String args[])
     {
+    	Scanner sc = new Scanner(System.in);
     	TicTacToeGame t = new TicTacToeGame();
     	//creating board
     	t.initializeBoard();
     	System.out.println("Board initalized");
+    	System.out.println("Enter value for player x or o");
+    	char player = sc.next().charAt(0);
+    	char computer = ' ';
+    	if(player == 'o' || player == 'x')
+    	computer=playerAndComputerChoice(player,computer);//giving alternate value
+    	else
+    		System.out.println("Invalid input");
+    	System.out.println("player choice is "+player);
+    	System.out.println("computer choice is "+computer);
+    	
     }
 }
